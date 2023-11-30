af3969b719a60aa0b545e9c460040007a6351b26
Daichi Kawashima <daichi0.0daichi@gmail.com>
2023-12-01 00:30:19 +0900

    fixed

---
+++ static/assets/config.js
@@ -0,0 +1,2 @@
+const GAS_ENDPOINT = 'https://script.google.com/macros/s/AKfycbwnelYi0vNupNIwmmBIIbUQRKrinP0fEuFn_V1SVJdVB9Kb9e6kleHFVilM1EXoREAzZA/exec';
+const TL_GAS = 'https://script.google.com/macros/s/AKfycbwFOh5d83nBv-5M2IEFxNkpq94Rk9j7RA5QrgieomHACKgI3BqLAGUMX5uG7mHAKpE5/exec';
